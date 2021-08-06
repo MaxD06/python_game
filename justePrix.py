95da3070655a53cbdea12ba2cd51373a6da14e45
Max Diep <max06.diep@gmail.com>
2021-08-06 19:10:35 +0200

    Firt lines in python

---
+++ justePrix.py
@@ -0,0 +1,8 @@
+#!/bin/python
+from random import *
+print("Debut du jeux : Le juste prix")
+print("Quel est le prix d'une voiture ?")
+prix=input("Saisissez le jsute prix :  ")
+prixcacheVoiture=randint(15000,40000)
+print("Prix cache de la voiture", prixcacheVoiture)
+print("Leprix que vous avez choisi est :", prix)
